4669d9afd8a1c0458ddbc8cd9017b694aaa59a55
Rohit Bhattacharyay <rohitbhattacharya260@gmail.com>
2026-06-07 12:32:33 +0530

    Add Firebase configuration and update gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,12 @@ import {
 // --- Firebase Configuration ---
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbUZPRoYKIo2yEB4x0pRbQm-Mrq_i9Rp4",
-  authDomain: "career-council-caf43.firebaseapp.com",
-  projectId: "career-council-caf43",
-  storageBucket: "career-council-caf43.firebasestorage.app",
-  messagingSenderId: "1021230419673",
-  appId: "1:1021230419673:web:1c1674f97a46ada8def8c8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 
